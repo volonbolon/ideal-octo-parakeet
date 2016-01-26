2e990d2170f18079997e9a4952a7092649305d8e
Ariel Rodriguez <ariel@volonbolon.me>
2016-01-26 11:03:46 -0300

    Add BookInfoViewController.swift

---
+++ IdealOctoParakeet/IdealOctoParakeet/BookInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BookInfoViewController.swift
+//  IdealOctoParakeet
+//
+//  Created by Ariel Rodriguez on 1/26/16.
+//  Copyright © 2016 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class BookInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
