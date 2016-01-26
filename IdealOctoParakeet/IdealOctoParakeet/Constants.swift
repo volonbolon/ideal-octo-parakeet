06534e687ac2aa6af533ba27506146483aa63d8b
Ariel Rodriguez <ariel@volonbolon.me>
2016-01-26 11:01:50 -0300

    Add Constants

---
+++ IdealOctoParakeet/IdealOctoParakeet/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  IdealOctoParakeet
+//
+//  Created by Ariel Rodriguez on 1/26/16.
+//  Copyright © 2016 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct SegueIdentifier {
+        static let ShowBookDetail = "ShowBookDetail"
+    }
+}
